9405b4c39e6945fcd5d1f802504855c6d575ed6a
LitanMolla <litanmolla9@gmail.com>
2026-09-07 21:13:13 +0600

    mongodb connected

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const dbConnection = async () => {
+    try {
+        const MONGO_URI = process.env.MONGO_URI
+        await mongoose.connect(MONGO_URI)
+        console.log('Database connected successfully')
+    } catch (error) {
+        console.log('Database connection error:', error.message)
+    }
+}
+
+module.exports = dbConnection
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
+require('node:dns').setServers(['1.1.1.1','8.8.8.8'])
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const dbConnection = require('./config/db')
 
 const app = express()
 
@@ -8,5 +11,6 @@ app.use(express.json())
 app.use(cors())
 
 const PORT = process.env.PORT || 8000
+dbConnection()
 
 app.listen(PORT,()=>{console.log(`Server is running on port: ${PORT}`)})
