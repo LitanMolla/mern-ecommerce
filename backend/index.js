d0801a44a03609149e244007e6562aba6504e93e
LitanMolla <litanmolla9@gmail.com>
2026-09-07 21:30:14 +0600

    added: routes

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const dbConnection = require('./config/db')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express()
 
 // middlewares
 app.use(express.json())
 app.use(cors())
+app.use('/api/v1/auth', authRoutes)
 
 const PORT = process.env.PORT || 8000
 dbConnection()
