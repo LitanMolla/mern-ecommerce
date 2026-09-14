0f24b575089ffaaf91870f5f50c0813cf389f38f
LITANMOLLA <litanmolla9@gmail.com>
2026-09-14 19:40:51 +0600

    added: reset pw route and controller

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -5,7 +5,7 @@
 
 const bcrypt = require('bcrypt')
 const User = require('../models/userModel');
-const {sendEmail, resetPasswordEmail} = require('../utils/sendEmail');
+const { sendEmail, resetPasswordEmail } = require('../utils/sendEmail');
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
 const jwt = require('jsonwebtoken');
@@ -57,7 +57,8 @@ const loginController = async (req, res) => {
         if (!passwordVerify) {
             return res.status(400).json({ success: false, message: 'Invalid Credential.' })
         }
-        return res.status(200).json({ success: true, message: 'Login success.', data: { _id: user._id, fullName: user.fullName } })
+        const token = generateAccessToken(user)
+        return res.status(200).json({ success: true, message: 'Login success.', data: { _id: user._id, fullName: user.fullName , email: user.email, role: user.role} , accessToken: token})
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message })
     }
@@ -86,24 +87,49 @@ const verifyAccountController = async (req, res) => {
 }
 
 
-const forgotPasswordController = async (req,res) => {
+const forgotPasswordController = async (req, res) => {
     try {
-        const {email} = req.body
+        const { email } = req.body
         if (!email) {
-            return res.status(400).json({ success: false, message: 'Email is required.'})
+            return res.status(400).json({ success: false, message: 'Email is required.' })
         }
-        const existingUser = await User.findOne({email})
+        const existingUser = await User.findOne({ email })
         if (!existingUser) {
-            return res.status(404).json({ success: false, message: 'User not found.'})
+            return res.status(404).json({ success: false, message: 'User not found.' })
         }
 
         const token = generateAccessToken(existingUser)
-        await resetPasswordEmail(email,token) 
-        return res.status(200).json({ success: true, message: 'Password resset email send successfully, please check your email.' })
+        await resetPasswordEmail(email, token)
+        return res.status(200).json({ success: true, message: 'Password reset email send successfully, please check your email.' })
     } catch (error) {
-        
+        return res.status(500).json({ success: false, message: error.message })
     }
 }
 
+const resetPasswordController = async (req, res) => {
+    try {
+        const { token } = req.params
+        const { password, confrimPassword } = req.body
+        if (!password || !confrimPassword) {
+            return res.status(400).json({ success: false, message: 'All feild are requred.' })
+        }
+        if (password !== confrimPassword) {
+            return res.status(400).json({ success: false, message: 'Password not matched.' })
+        }
+        if (!token) {
+            return res.status(404).json({ success: false, message: 'Token not found.' })
+        }
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+        const existingUser = await User.findById(decode._id)
+        if (!existingUser) {
+            return res.status(404).json({ success: false, message: 'User not found.' })
+        }
+        const hashPassword = bcrypt.hashSync(password, 10)
+        const updateUser = await User.findByIdAndUpdate(decode._id, { password: hashPassword }, { new: true }).select('-password')
+        return res.status(200).json({ success: true, message: 'Password reset successfully' , data: updateUser})
+    } catch (error) {
+        return res.status(400).json({ success: false, message: 'Invalid token or token expired', })
+    }
+}
 
-module.exports = { registerController, loginController, verifyAccountController, forgotPasswordController }
+module.exports = { registerController, loginController, verifyAccountController, forgotPasswordController, resetPasswordController }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { registerController, loginController, verifyAccountController, forgotPasswordController } = require('../controllers/authControllers')
+const { registerController, loginController, verifyAccountController, forgotPasswordController, resetPasswordController } = require('../controllers/authControllers')
 const router = express.Router()
 
 router.post('/register', registerController)
 router.post('/login', loginController)
 router.get('/verify/:token', verifyAccountController)
 router.post('/forgot-password', forgotPasswordController)
+router.post('/reset-password/:token', resetPasswordController)
 
 module.exports = router
