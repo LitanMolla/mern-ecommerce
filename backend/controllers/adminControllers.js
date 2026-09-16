319184f2bf26ac41dd0d89252c8578422573c345
LitanMolla <litanmolla9@gmail.com>
2026-09-17 00:45:24 +0600

    added: deleted controller

--- backend/controllers/adminControllers.js
+++ backend/controllers/adminControllers.js
@@ -19,7 +19,31 @@ const getAllUsersController = async (req, res) => {
     try {
         const users = await User.find({}).select('-password')
         return res.status(200).json({
-            succuss: true, 
+            succuss: true,
+            message: `Total ${users.length} user found`,
+            data: users
+        })
+    } catch (error) {
+        return res.status(500).json({ status: false, message: 'Internel server error' })
+    }
+}
+const getAllActiveUsersController = async (req, res) => {
+    try {
+        const users = await User.find({status: 'active'}).select('-password')
+        return res.status(200).json({
+            succuss: true,
+            message: `Total ${users.length} user found`,
+            data: users
+        })
+    } catch (error) {
+        return res.status(500).json({ status: false, message: 'Internel server error' })
+    }
+}
+const getAllDeactiveUsersController = async (req, res) => {
+    try {
+        const users = await User.find({status: 'suspended'}).select('-password')
+        return res.status(200).json({
+            succuss: true,
             message: `Total ${users.length} user found`,
             data: users
         })
@@ -28,5 +52,42 @@ const getAllUsersController = async (req, res) => {
     }
 }
 
+const getUserController = async (req, res) => {
+    try {
+        const { id } = req.params
+        if (!id) {
+            return req.status(400).json({ status: false, message: 'User id required' })
+        }
+        const user = await User.findById(id).select('-password')
+        if (!user) {
+            return res.status(400).json({ status: false, message: 'User not found' })
+        }
+        return res.status(200).json({ success: true, message: 'User find successfully', data: user })
+    } catch (error) {
+        return res.status(500).json({ status: false, message: 'Internel server error' })
+    }
+}
+const deleteUserController = async (req, res) => {
+    try {
+        const { id } = req.params
+        if (!id) {
+            return req.status(400).json({ status: false, message: 'User id required' })
+        }
+        const user = await User.findByIdAndDelete(id)
+        if (!user) {
+            return res.status(400).json({ status: false, message: 'User not found' })
+        }
+        return res.status(200).json({ success: true, message: 'User deleted successfully' })
+    } catch (error) {
+        return res.status(500).json({ status: false, message: 'Internel server error' })
+    }
+}
 
-module.exports = { userUpdateController, getAllUsersController }
+module.exports = {
+    userUpdateController,
+    getAllUsersController,
+    getUserController,
+    getAllActiveUsersController,
+    getAllDeactiveUsersController,
+    deleteUserController
+}
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const { userUpdateController, getAllUsersController } = require('../controllers/adminControllers')
+const { userUpdateController, getAllUsersController, getUserController, getAllActiveUsersController, getAllDeactiveUsersController } = require('../controllers/adminControllers')
 const router = express.Router()
 
 router.post('/update/:id', userUpdateController)
 router.get('/all-users', getAllUsersController)
+router.get('/user/:id', getUserController)
+router.get('/active-users', getAllActiveUsersController)
+router.get('/deactive-users', getAllDeactiveUsersController)
 
 module.exports = router
