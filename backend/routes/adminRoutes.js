fcdcd2c0a28ffd9f050b0a752ec24f429571dc56
LITANMOLLA <litanmolla9@gmail.com>
2026-09-17 17:39:53 +0600

    added: user delete route and controller

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express')
-const { userUpdateController, getAllUsersController, getUserController, getAllActiveUsersController, getAllDeactiveUsersController } = require('../controllers/adminControllers')
+const {
+    userUpdateController,
+    getAllUsersController,
+    getUserController,
+    getAllActiveUsersController,
+    getAllDeactiveUsersController,
+    deleteUserController
+} = require('../controllers/adminControllers')
+
 const router = express.Router()
 
 router.post('/update/:id', userUpdateController)
+router.delete('/delete/:id', deleteUserController)
 router.get('/all-users', getAllUsersController)
 router.get('/user/:id', getUserController)
 router.get('/active-users', getAllActiveUsersController)
