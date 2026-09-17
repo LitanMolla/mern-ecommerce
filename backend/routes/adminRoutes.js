832f684aeef0853cde2ccc24ae9deba11f35e604
LITANMOLLA <litanmolla9@gmail.com>
2026-09-17 19:29:45 +0600

    updated rout serial

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -13,14 +13,14 @@ const {
 
 const router = express.Router()
 
-router.post('/update/:id', userUpdateController)
-router.post('/update-category/:id', updateCategory)
-router.post('/delete-category/:id', categoryDeleteController)
-router.get('/categories', getAllCategories)
-router.delete('/delete/:id', deleteUserController)
 router.get('/all-users', getAllUsersController)
 router.get('/user/:id', getUserController)
 router.get('/active-users', getAllActiveUsersController)
+router.post('/update/:id', userUpdateController)
 router.get('/deactive-users', getAllDeactiveUsersController)
+router.delete('/delete/:id', deleteUserController)
+router.get('/categories', getAllCategories)
+router.post('/update-category/:id', updateCategory)
+router.post('/delete-category/:id', categoryDeleteController)
 
 module.exports = router
